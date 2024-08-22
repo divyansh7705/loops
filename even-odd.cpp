0b8bc7bcb94b19d8f3f61f3eea2fcb10995d7636
divyansh7705 <divyanshrastogi91@gmail.com>
2024-08-22 23:43:40 +0530

    Add files via upload

---
+++ even-odd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "enter a number:";
+    cin >> n;
+    if (n%2==0)
+    {
+        cout << "this is a even number";
+    }else{
+        cout << "this is a odd number";
+    }
+    return 0;
+}
